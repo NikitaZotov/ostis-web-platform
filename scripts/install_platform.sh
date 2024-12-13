892c61409fcd12532137fefdbfce76fbffd40580
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-12-13 19:48:34 +0300

    scripts: remove install deps, build and run scripts

--- scripts/install_platform.sh
+++ scripts/install_platform.sh
@@ -7,11 +7,5 @@ source "${SCRIPTS_DIR}/formats.sh"
 stage "Install ostis-web-platform"
 
 "${SCRIPTS_DIR}/install_submodules.sh"
-"${SCRIPTS_DIR}/install_dependencies.sh" --dev
-"${SCRIPTS_DIR}/build_sc_machine.sh"
-"${SCRIPTS_DIR}/build_scp_machine.sh"
-"${SCRIPTS_DIR}/build_sc_component_manager.sh"
-"${SCRIPTS_DIR}/build_sc_web.sh"
-"${SCRIPTS_DIR}/build_kb.sh"
 
 stage "ostis-web-platform is installed successfully"
