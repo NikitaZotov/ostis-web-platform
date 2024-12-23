262882ade8e002fdf906f3089bc1ad0f9ebc93b5
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-12-23 15:40:56 +0300

    scripts: remove install platform scripts

--- scripts/install_minimal_submodules.sh
+++ scripts/install_minimal_submodules.sh
@@ -5,10 +5,10 @@ SCRIPTS_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 SUBMODULE_SCRIPTS_DIR="${SCRIPTS_DIR}/submodule-scripts"
 source "${SCRIPTS_DIR}/formats.sh"
 
-stage "Install ostis-web-platform"
+stage "Clone submodules"
 
 "${SUBMODULE_SCRIPTS_DIR}/install_sc_machine.sh"
 "${SUBMODULE_SCRIPTS_DIR}/install_scp_machine.sh"
 "${SUBMODULE_SCRIPTS_DIR}/install_sc_component_manager.sh"
 
-stage "ostis-web-platform is installed successfully"
+stage "Submodules cloned successfully"
